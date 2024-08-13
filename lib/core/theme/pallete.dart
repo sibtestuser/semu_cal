c1669f71abfba288cbe8fca8b686b75d23ead0ef
jungwoo seo <jungwooseo@jungwooui-MacBookPro.local>
2024-08-14 00:33:38 +0900

    First commit

---
+++ lib/core/theme/pallete.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const blackColor = Color.fromRGBO(1, 1, 1, 1); // primary color
+  static const greyColor = Color.fromRGBO(26, 39, 45, 1); // secondary color
+  static const drawerColor = Color.fromRGBO(18, 18, 18, 1);
+  static const whiteColor = Colors.white;
+  static var redColor = Colors.red.shade500;
+  static var blueColor = Colors.blue.shade300;
+  static var darkGreyColor = Colors.grey.shade800;
+  static var lightGreyColor = Colors.grey.shade400;
+  static var verylightGreyColor = Colors.grey.shade100;
+  static var metalgreyColor = Colors.grey.shade500;
+  static var pastelyellowColor = Colors.yellow.shade100;
+  static var pastelredColor = Colors.red.shade300;
+
+  // Themes
+  static var darkModeAppTheme = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: blackColor,
+    cardColor: greyColor,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: drawerColor,
+      iconTheme: IconThemeData(
+        color: whiteColor,
+      ),
+    ),
+    
+    // backgroundColor: drawerColor, // will be used as alternative background color
+  );
+
+  static var lightModeAppTheme = ThemeData.light().copyWith(
+    scaffoldBackgroundColor: whiteColor,
+    cardColor: greyColor,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: whiteColor,
+      elevation: 0,
+      iconTheme: IconThemeData(
+        color: blackColor,
+      ),
+    ),
+    
+    //backgroundColor: whiteColor,
+  );
+}
---
+++ lib/core/theme/texttheme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CustomTextTheme{
+  static final outputText_black =  TextStyle(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.black);  
+  static final outputText_white = TextStyle(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.white);  
+
+  static final button_text_black = TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black);
+  static final button_text_white = TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white);
+
+  static final button_text_grey = TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.grey); 
+
+  static final dialog_text_red = TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.red);
+
+  static final guide_text_black = TextStyle(fontSize: 15,  color: Colors.black);  
+  static final guide_text_white = TextStyle(fontSize: 15,  color: Colors.white);  
+  static final guide_text_blue = TextStyle(fontSize: 15,  color: Colors.blue);
+  static final guide_text_red = TextStyle(fontSize: 15,  color: Colors.red);      
+}
